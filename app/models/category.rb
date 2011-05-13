6483f48fbeb17dc4d17ae89bb673003485737d10
ddarino@gmail.com <damian.darino@labanca.com.uy>
2011-05-13 11:51:57 -0300

    Cliente REST de prueba para el proyecto labanca-ads del curso

---
+++ app/models/category.rb
@@ -0,0 +1,3 @@
+class Category < ActiveResource::Base
+	self.site = 'http://localhost:3000/'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestAr::Application.config.secret_token = '8f66facae5443dc5111f9e74a9d9c2c44626d40fd739217143a9aa9f66d7d43c70676885a4b902775c20045a1be0f537bba8c2c333c93e59c466773d9b7ff421'
